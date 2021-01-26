17c990a896f3b8ffe15517459f512ca3acb7045d
fanicahyo7 <fanidc7@gmail.com>
2021-01-26 19:55:57 +0700

    theme

---
+++ lib/theme.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+Color purpleColor = Color(0xff5843BE);
+Color whiteColor = Color(0xffFFFFFF);
+Color blackColor = Color(0xff000000);
+Color greyColor = Color(0xff82868E);
+Color orangeColor = Color(0xffFF9376);
+
+TextStyle poppinsMedium = GoogleFonts.poppins(
+  fontWeight: FontWeight.w500
+);
+
+TextStyle poppinsLight = GoogleFonts.poppins(
+  fontWeight: FontWeight.w300,
+  color: greyColor,
+);
